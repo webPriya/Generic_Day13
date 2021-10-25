975f0c76f87b0e13f7234a179310f87a26e17a70
webPriya <priyachouhan1019@gmail.com>
2021-10-25 01:13:45 -0700

    find maximum float values at mentioned  position

--- src/test/java/com/bdlz/findMax/FindMaximumTest.java
+++ src/test/java/com/bdlz/findMax/FindMaximumTest.java
@@ -14,12 +14,12 @@ public class FindMaximumTest {
     @Test
     public void findMaximum_atSecondPosition() {
         float actualResult = FindMaximum.maximum(6.5f,9.2f,3.2f);
-        Assert.assertEquals(6.5,actualResult,0.0);
+        Assert.assertEquals(9.2,actualResult,0.0);
     }
     @Test
     public void findMaximum_atThirdPosition() {
         float actualResult = FindMaximum.maximum(6.5f,1.2f,9.2f);
-        Assert.assertEquals(6.5,actualResult,0.0);
+        Assert.assertEquals(9.2,actualResult,0.0);
     }
 
 
