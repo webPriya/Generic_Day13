46061d0484842dc8be658dbcb238d9ffa86cd902
webPriya <priyachouhan1019@gmail.com>
2021-10-25 00:43:22 -0700

    find maximum value at second posision

--- src/test/java/com/bdlz/findMax/FindMaximumTest.java
+++ src/test/java/com/bdlz/findMax/FindMaximumTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class FindMaximumTest {
     @Test
     public void findMaximum_atFirstPosition() {
-    int actualResult = FindMaximum.maximum(6,3,1);
+    int actualResult = FindMaximum.maximum(3,6,1);
         Assert.assertEquals(6,actualResult);
     }
 
